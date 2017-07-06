2e17edd3b5f3f7dda12c61424a99b84fc96d793b
Alex Alonso <alex4551@gmail.com>
2017-07-06 14:20:42 +0800

    add tslint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-import express = require('express');
+import express = require("express");
 import {Server} from "./server";
 
 const app: express.Application = Server.bootstrap();
 
-app.get('/', (req:any, res:any) => {
-  res.send('Hello World')
+app.get("/", (req: any, res: any) => {
+  res.send("Hello World");
 });
--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,15 @@
-import express = require('express');
+import express = require("express");
 
-export class Server{
-    public app: express.Application;
-
+export class Server {
     public static bootstrap(): express.Application {
         return new Server(3000).app;
     }
-
-    private constructor(port:Number){
+    public app: express.Application;
+    private constructor(port: number) {
         this.app = express();
         this.app.listen(port, () => {
-            console.log('listening on '+port)
+            /* tslint:disable:no-console */
+            console.log("listening on " + port);
         });
     }
 }
