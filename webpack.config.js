1e58e970fcce3723b8c30554a562d2811ddabb20
Alex Alonso <alex4551@gmail.com>
2017-07-06 16:57:41 +0800

    add eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,36 @@
-var path = require('path');
-var webPack = require('webpack');
+const path = require("path");
+// const webPack = require('webpack');
 
 module.exports = {
-    target: 'node',
-    entry: './src/index.ts',
-    output: {
-        filename: 'index.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    resolve: {
-        extensions: [ ".ts", ".js"]
-    },
-    module: {
-        rules:[
-        {
-            enforce: 'pre',
-            test: /\.tsx?$/,
-            loader: 'tslint-loader',
-            exclude: /node_modules/,
-            options:{
-                emitErrors: false,
-                failOnHint: true,
-                tsConfigFile: 'tsconfig.json',
-            }
+  target: "node",
+  entry: "./src/index.ts",
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  resolve: {
+    extensions: [".ts", ".js"],
+  },
+  module: {
+    rules: [
+      {
+        enforce: "pre",
+        test: /\.tsx?$/,
+        loader: "tslint-loader",
+        exclude: /node_modules/,
+        options: {
+          emitErrors: false,
+          failOnHint: true,
+          tsConfigFile: "tsconfig.json",
         },
-        { 
-            test: /\.tsx?$/,
-            loader: 'awesome-typescript-loader',
-        }
-        ]
-    },
-    plugins:[
-        // new webPack.optimize.UglifyJsPlugin()
-    ]
-}
+      },
+      {
+        test: /\.tsx?$/,
+        loader: "awesome-typescript-loader",
+      },
+    ],
+  },
+  plugins: [
+  // new webPack.optimize.UglifyJsPlugin()
+  ],
+};
